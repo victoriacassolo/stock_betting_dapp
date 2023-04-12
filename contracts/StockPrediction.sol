d675d66002ceb5043fc45e3f87663e7d3f5b4a75
victoriacassolo <vitoria.cassolo@gmail.com>
2023-04-12 17:05:25 -0300

    first commit

---
+++ contracts/StockPrediction.sol
@@ -0,0 +1,8 @@
+// SDPX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract StockPrediction {
+    address public owner;
+    uint256 public predictionStartTime;
+    uint256 public predictionEndTime;
+}
